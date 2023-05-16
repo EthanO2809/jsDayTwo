17b7a6c0b7ac893ff480cd68b15b9d51963a3635
EthanO2809 <130046659+EthanO2809@users.noreply.github.com>
2023-05-16 16:23:54 +0200

    Add files via upload

---
+++ script/code.js
@@ -0,0 +1,11 @@
+const Subjects= [
+    'Python',
+    'C++',
+    'VB.Net',
+    'JavaScript',
+    'Assembly',
+]
+
+console.log(Subjects[Subjects.length-1]);
+
+console.log(Subjects[Subjects.length-3]);
